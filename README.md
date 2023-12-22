842142963357e7fe5c4a9f84c4c616223317725b
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2023-12-22 03:34:28 -0500

    comments, README, final tweaks

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Logo-Generator
+  [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+
+## Description
+
+Need a logo? Pesky designers definitely charging too much for creative labor that coders definitely would be able to replicate with automation? Boy, do I have the handy little tool for you!
+
+In all seriousness, SVGs are a fun and unique challenge to program and implement, and they can help generate ideas for more polished graphics. This program helps engage the creative flow by generating some logos with user input through the command line. This project challenged me to utilize paradigms for more advanced code in Object-Oriented Programming and Test Driven Development. Furthering my knowledge of NodeJS, this program also uses Jest for test suites. I learned a lot about how to tackle projects in a structured way, through pseudocode and diagrams, before diving in. All in all, I'm happy with how this turned out, and feel confident with errors, tests, and class structures because of it.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributions](#contributions)
+- [Testing](#testing)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+This program uses a few different applications. Git Bash, NodeJS, and the Inquirer package within NodeJS. Once those are installed, download this repository.
+
+## Usage
+[Walkthrough Video](https://youtu.be/UcWbsejf59E)
+
+This application is very straight-forward and easy to use. When in the project folder in the command line, use "node index.js" as a command to begin. After this, input text after the prompts or choose from a list using the arrow keys and the enter button. The logo file will be added to the folder when finished.
+
+Be aware, using the program again will overwrite any files of the same name "logo.svg" in that folder, so be sure to save a copy of each logo you make before starting again.
+
+## Contributions
+If you wish to contribute to this project, please include this simple statement below with any non-trivial patches. 
+
+`I dedicate any and all copyright interest in this software to the public domain. I make this dedication for the benefit of the public at large and to the detriment of my heirs and successors. I intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.`
+
+## Testing
+
+Testing requires the dev dependency package jest for NodeJS. Installing this, configuring the test files (with the structure 'something.test.js' in the lib path) and running the command 'npm test' should allow you to test the program. When attempting to create improvements or new implementations, these tests will ensure the file is still maintaining its prior functionality.
+
+## License
+[The Unlicense](http://unlicense.org/)
+
+This software operates under The Unlicense. If you would like to learn more, please click on the link above.
+
+## Questions
+[Github](https://github.com/RedKnight88)
+
+Email: ryansauter88@gmail.com
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+// imports packages for methods used later, as well as imports the shape classes
 const shapes = require('./lib/shapes.js');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+// a couple other variables that are used during the error checks
+// declaring the variables up here makes the code easier to change and 
+// cleaner within the function, even if these are only used in one function
 const colors = ["aqua", "black", "blue", "fuchsia", "gray", "green", "lime", "maroon", "navy", "olive", "purple", "red", "silver", "teal", "white", "yellow"];
-const hexPattern = new RegExp('(^[A-F0-9]+$)');
+const hexPattern = new RegExp('(^[A-F0-9]+$)'); // this ensures strings contain only characters included in hexidecimal
 
+// initiates the prompt functionality for the command line
+// this intakes user inputs for the variable components of the SVG (text, text color, logo shape, logo color)
 function initiatePrompts() {
     inquirer
         .prompt([
@@ -30,6 +36,10 @@ function initiatePrompts() {
                 name: "logoColor",
             },
         ])
+        // Once the inputs are received, the response object should contain the data we need to generate the file
+        // So, a function checks for errors in the inputs, and throws an error if anything was input in an improper formatting
+        // Then, we call a function to make a string containing the SVG formatting
+        // Finally, we write that string into a file with the .svg extension, and log the completion of the program's task
         .then((response) => {
             errorChecker(response);
             const newSVG = generateSVG(response);
@@ -39,26 +49,41 @@ function initiatePrompts() {
         });
 }
 
+// My reasoning for keeping the error messages within this function are two-fold.
+// First, it allows an easier way for the testing structure to interface with the error throws.
+// Second, it means the response data does not require getting passed between scripts,
+// which I prefer in order to maintain a good logic flow while working.
 function errorChecker(inputs) {
+    // To start, I check the logo text string, ensuring it stays within the limit
     if(inputs.logoText.length > 3) {
         throw new Error('ERROR: Please only input three or less characters')
     }
 
+    // This is a bit funky, but it ensures that the input color matches one of the presets built into HTML by filtering through the color array to find a match
+    // and returning a boolean (just the filter function would only pass the keyword, so I put an equality operator comparing the keyword to itself)
     let isTextKeyword = inputs.textColor == colors.filter((color) => {return color == inputs.textColor;})
     let isLogoKeyword = inputs.logoColor == colors.filter((color) => {return color == inputs.logoColor;})
 
+    // This tests the input color to ensure it contains only hexidecimal compatible characters and is 6 characters long, which is what is necessary for reading colors through hexidecimal
     let isTextHex = hexPattern.test(inputs.textColor) && inputs.textColor.length == 6;
     let isLogoHex = hexPattern.test(inputs.logoColor) && inputs.logoColor.length == 6;
 
+    // Then, only if both the hexidecimal and keyword tests fail, an error is thrown (checked for both the text color and logo color)
     if(!isTextHex && !isTextKeyword) {
         throw new Error('ERROR: Please input the text color as either a hexadecimal value (no #) or a common color (in lowercase).')
     }
     if(!isLogoHex && !isLogoKeyword) {
         throw new Error('ERROR: Please input the logo color as either a hexadecimal value (no #) or a common color (in lowercase).')
     }
+    // if all is well, it returns and function exits with no information passed back
     return;
 }
 
+
+/** This function selects the shape through a switch function (since the shape input is a list, it's guaranteed
+ * to be one of these three choices). Once the shape is selected and the color from the input data is set,
+ * all of the necessary components are slotted into the svg framework string literal. This string is returned.
+ */
 function generateSVG(data) {
     let shape;
     switch(data.logoShape) {
@@ -86,6 +111,8 @@ function generateSVG(data) {
 </svg>`
 }
 
+// Can't start the prompts without calling the function
 initiatePrompts();
 
+// Exports the error checker function so the tests can engage with it
 module.exports = errorChecker;
